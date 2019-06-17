4420fe1f49cb187ef5f725344cca76611a018008
Japhet <japhetmesa@gmail.com>
2019-06-17 10:28:19 +0200

    Create nodeTest.js

---
+++ nodeTest.js
@@ -0,0 +1,7 @@
+const name = "Japhet"
+const otherName = "Mesa"
+const earthBornDate = "1000000"
+const yearToDay = "2019"
+let earthAge = earthbornDate - yearToDay
+console.log("Am" + name + " " + otherName + " and I live on earth which I think is " + earthAge)
+
